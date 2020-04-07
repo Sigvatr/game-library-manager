36374a92d91bf8a1941a0f0fd5e9d4e7c2bc0b25
Sigvatr <sigvatr@wp.pl>
2020-04-07 19:29:31 +0200

    Fixing and testing code for downloading request from Steam API with cache

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import json
 import requests
 import hashlib
-from time import gmtime
+import time
+import csv
 
 
 def read_json_file(file_path: str) -> dict:
@@ -15,23 +16,28 @@ def load_configuration_file(config_file_path: str) -> dict:
 def to_md5(value: str) -> str:
     return hashlib.md5(value.encode('utf-8')).hexdigest()
 
+def cache_file(cache_name: str):
+    return f'.cache/{cache_name}.json'
+
 def cache_exist(cache_name: str) -> bool:
-    return os.path.isfile(f'.cache/{cache_name}')
+    return os.path.isfile(cache_file(cache_name))
 
 def is_cache_fresh(cache_name: str) -> bool:
-    return os.path.getmtime(f'.cache/{cache_name}') + 24 * 60 * 60 < gmtime()
+    return (os.path.getmtime(cache_file(cache_name)) + 24 * 60 * 60) > time.time()
 
 def cache_exist_and_is_fresh(cache_name) -> bool:
     return cache_exist(cache_name) and is_cache_fresh(cache_name)
 
 def read_cache_file(cache_name: str) -> bool:
-    return read_cache_file(cache_name)
+    print('Reading from cache:', cache_name)
+    return read_json_file(cache_file(cache_name))
 
 def execute_request(url: str) -> str:
+    print('Online request:', url)
     return requests.get(url).json()
 
 def save_cache(cache_name: str, content: str):
-    with open(f'.cache/{cache_name}.json', 'w') as file:
+    with open(cache_file(cache_name), 'w') as file:
         json.dump(content, file)
 
 def pass_and_save_response(url: str, cache_name: str) -> str:
@@ -41,23 +47,15 @@ def pass_and_save_response(url: str, cache_name: str) -> str:
     
 def get_data(url: str) -> str:
     cache_name = to_md5(url)
-
     return read_cache_file(cache_name) if cache_exist_and_is_fresh(cache_name) else pass_and_save_response(url, cache_name)
 
 def api_get_own_games(steam_api_key: str):
     return lambda steam_user_id: f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={steam_api_key}&steamid={steam_user_id}&format=json&include_appinfo=true'
 
-class Game():
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
-
-    def __str__(self):
-        return str(self.__dict__)
-
-def repack_games_data(steam_games):
-    return (Game(**game) for game in steam_games['response']['games'])
+def get_only_games_data(steam_games):
+    return steam_games['response']['games']
 
 settings = load_configuration_file('settings.json')
-games = repack_games_data(get_data(api_get_own_games(settings['steam']['api_key'])(settings['steam']['user_id'])))
-x = next(games)
-print(x)
+games = get_only_games_data(get_data(api_get_own_games(settings['steam']['api_key'])(settings['steam']['user_id'])))
+game = games[0]
+print(game)
